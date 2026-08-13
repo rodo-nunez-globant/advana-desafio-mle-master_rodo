7273fac078a785cae15fedabf0e6b413bd9e47b2
Rodo <rodolfo.nunez@globant.com>
2026-08-13 18:12:26 -0400

    we only care about detecting 1s with hard metrics. A model in the future could be good for both, but out objective is to detect delayed flights

--- tests/model/test_model.py
+++ tests/model/test_model.py
@@ -81,8 +81,6 @@ class TestModel(unittest.TestCase):
 
         report = classification_report(target_validation, predicted_target, output_dict=True)
         
-        assert report["0"]["recall"] < 0.60
-        assert report["0"]["f1-score"] < 0.70
         assert report["1"]["recall"] > 0.60
         assert report["1"]["f1-score"] > 0.30
 
