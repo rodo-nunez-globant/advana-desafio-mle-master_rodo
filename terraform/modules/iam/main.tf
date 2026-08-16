ac332ba1af0db5f7f6f48afe7c96818dfeaee7f8
Rodo <rodolfo.nunez@globant.com>
2026-08-16 04:32:31 -0400

    testing new permission for tf apply

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -77,7 +77,20 @@ resource "google_service_account_iam_binding" "self_impersonation" {
   count = var.enable_self_impersonation ? 1 : 0
 
   service_account_id = "projects/${var.project_id}/serviceAccounts/${var.service_account_email}"
-  role               = "roles/iam.workloadIdentityUser"
+  role               = "roles/iam.serviceAccountTokenCreator"
+
+  members = [
+    local.service_account_member
+  ]
+}
+
+# Grant service account user role at project level
+# This is required for Terraform GCS backend authentication with Workload Identity
+resource "google_project_iam_binding" "service_account_token_creator" {
+  count = var.enable_self_impersonation ? 1 : 0
+
+  project = var.project_id
+  role    = "roles/iam.serviceAccountTokenCreator"
 
   members = [
     local.service_account_member
